3bf56094cbfa105778ad34a3ff9ab1608990ee4f
guilhermespirlandelli <guilherme.spirlandelli01@fatec.sp.gov.br>
2023-01-18 10:29:40 -0300

    first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #27282F;
+    color: #fff;
+    min-height: 100vh;
+`;
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding:
+`;
+
+export const Header = styled.h1`
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    margim-bottom: 30px:
+`;
