25c36c7780cf61ef38d03a9145228b7ff3b174d2
guilhermespirlandelli <guilherme.spirlandelli01@fatec.sp.gov.br>
2023-01-18 11:43:18 -0300

    CommitPart2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
+import { useState, useEffect } from "react";
 import * as C from "./App.styles";
+import * as Photos from "./services/photos";
+import { Photo } from "./types/Photo";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    };
+    getPhotos();
+  }, []);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Galeria de Fotos</C.Header>
 
         {/* Área de upload */}
-        {/* Galeria de fotos */}  
 
-        {/* PAREI NO 00:30:00 */ }
+        {/* Galeria de fotos */}
+
+        {loading && (
+          <C.ScreenWarning>
+            <div className="emoji">⏲️</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        )}
+
+        {!loading && photos.length > 0 && (
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotoList>
+        )}
+
+        {!loading && photos.length === 0 &&
+             <C.ScreenWarning>
+             <div className="emoji">😞</div>
+             <div>Não possui fotos cadastradas...</div>
+           </C.ScreenWarning>
+        }
+
+        {/* PAREI NO 00:57:00 */}
       </C.Area>
     </C.Container>
   );
